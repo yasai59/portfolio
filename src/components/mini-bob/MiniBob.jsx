7168991847f3229c39e608c4c90d33f573e6ef63
Yasai <alexvidalcasado@gmail.com>
2023-12-03 21:25:13 +0100

    [UPDATE] mini-bob and technologies

--- src/components/mini-bob/MiniBob.jsx
+++ src/components/mini-bob/MiniBob.jsx
@@ -1,5 +1,12 @@
 import "./MiniBob.css";
 
-export const MiniBob = () => {
-  return <div className="mini-bob"></div>;
+export const MiniBob = ({ text, href }) => {
+  return (
+    <div className={`mini-bob ${href ? "mini-bob-link" : ""}`}>
+      <div className="mini-bob-content">{text}</div>
+      <div className="mini-line mini-line-1"></div>
+      <div className="mini-line mini-line-2"></div>
+      <div className="mini-line mini-line-3"></div>
+    </div>
+  );
 };
--- src/sections/Technologies.jsx
+++ src/sections/Technologies.jsx
@@ -3,8 +3,15 @@ import { MiniBob } from "../components/mini-bob/MiniBob";
 
 export const Technologies = () => {
   return (
-    <div className="h-screen relative section snap-center">
-      <MiniBob />
+    <div className="h-screen relative section snap-center grid grid-cols-2 place-items-center">
+      <div className="grid grid-cols-2 place-items-center h-[80%] w-full ms-10">
+        <MiniBob text={"React, Vue, Angular"} />
+        <MiniBob text={"Node, Express, Nest"} />
+        <MiniBob text={"HTML + CSS+ Tailwind"} />
+        <MiniBob text={"NextJS, Laravel"} />
+        <MiniBob text={"SQL, noSQL"} />
+        <MiniBob text={"JavaScript, PHP"} />
+      </div>
     </div>
   );
 };
