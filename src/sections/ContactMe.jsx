555fc62dfbab5a55c09b11ee73dedc44998b1d0b
Yasai <alexvidalcasado@gmail.com>
2023-12-05 13:30:18 +0100

    [ADD] cookie

--- src/sections/ContactMe.jsx
+++ src/sections/ContactMe.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export const ContactMe = () => {
-  return <div className="h-screen snap-center"></div>;
+  const handleClick = () => {};
+
+  return (
+    <div className="h-screen snap-center relative">
+      <div className="give-a-cookie flex flex-col items-center">
+        <p className="pixel text-white text-center text-2xl">
+          Give Bob a cookie
+        </p>
+        <img
+          src="/assets/cookie.png"
+          style={{
+            width: "150px",
+          }}
+        />
+      </div>
+    </div>
+  );
 };
