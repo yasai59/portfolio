301892172fae2cd5c30530de05ff708a9d6feb04
Yasai <alexvidalcasado@gmail.com>
2024-01-30 15:23:04 +0100

    [ADD] bob eats cookies

--- src/sections/ContactMe.jsx
+++ src/sections/ContactMe.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useRef } from "react";
 
 export const ContactMe = () => {
-  const handleClick = () => {};
+  const divRef = useRef(null);
+
+  const handleClick = (e) => {
+    const audio = new Audio("/assets/eat.mp3");
+    const cookieEat = document.createElement("img");
+    cookieEat.src = "/assets/cookie.png";
+    cookieEat.classList.add("gotoCenter");
+    setTimeout(() => {
+      audio.play();
+      cookieEat.remove();
+    }, 400);
+
+    divRef.current.appendChild(cookieEat);
+  };
 
   return (
     <div className="h-screen snap-center relative">
@@ -9,12 +23,15 @@ export const ContactMe = () => {
         <p className="pixel text-white text-center text-2xl">
           Give Bob a cookie
         </p>
-        <img
-          src="/assets/cookie.png"
-          style={{
-            width: "150px",
-          }}
-        />
+        <div ref={divRef}>
+          <img
+            src="/assets/cookie.png"
+            style={{
+              width: "150px",
+            }}
+            onClick={handleClick}
+          />
+        </div>
       </div>
     </div>
   );
