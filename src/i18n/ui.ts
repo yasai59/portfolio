48c622b65d0f6300c11528de3e4a046c8bb1cd35
Yasai <alexvidalcasado@gmail.com>
2025-10-11 15:01:19 +0200

    [ADD] translation routing

---
+++ src/i18n/ui.ts
@@ -0,0 +1,26 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+  ca: "Català",
+  jp: "日本語",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "footer.contact.text":
+      "If you would like to get in touch with me, feel free to use the following methods",
+  },
+  es: {
+    "footer.contact.text":
+      "Si te gustaria ponerte en contacto conmigo, sientete libre de contactarme por aqui",
+  },
+  ca: {
+    "footer.contact.text":
+      "Si voldries contactar amb mi, sent-te lliure de contactar-me per aqui",
+  },
+  jp: {
+    "footer.contact.text": "私とつながりたかったら、次の選択肢を使ってください",
+  },
+} as const;
