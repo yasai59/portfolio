c70b5c3d55752b58597fcc6b0e664d543fb903de
Yasai <alexvidalcasado@gmail.com>
2025-10-20 12:34:41 +0200

    [EDIT] japanese string

--- src/i18n/langs/ja.ts
+++ src/i18n/langs/ja.ts
@@ -1,4 +1,4 @@
-import en from './en.js';
+import en from "./en.js";
 
 const jp: typeof en = {
   "general.name": "アレハンドロ",
@@ -11,7 +11,8 @@ const jp: typeof en = {
   "nav.blog": "ブログ",
   "nav.about": "私のこと",
   "nav.studies": "学歴",
-  "nav.clickToClose": "メニューを閉じるのにこのエリアの外をクリックしてください",
+  "nav.clickToClose":
+    "メニューを閉じるのにこのエリアの外をクリックしてください",
   "hero.title": "FIBで大学生",
   "hero.scrollText": "下にスクロールして続行",
   "blog.title": "ブログ",
@@ -24,12 +25,12 @@ const jp: typeof en = {
   "studies.SMIX": "コンピュータシステムとネットワークの専門課程",
   "studies.bobsays": "詳細を見るためにリスト項目にマウスを合わせて下さい",
   "studies.CS.detail": "今これを勉強していて大好きだ！",
-  "studies.DAW.detail": "カタロニアの最強の成績 - GPA: 3.98",
+  "studies.DAW.detail": "カタロニアの一番高い成績 - GPA: 3.98",
   "studies.SMIX.detail": "居室の一番!",
   "contact.title": "連絡",
-  "footer.contact.text":
-    "私に連絡したかったら次の選択肢を使ってください",
-  "footer.title": "ウェブ開発者 <br/> & 大学生"
+  "footer.contact.text": "私に連絡したかったら次の選択肢を使ってください",
+  "footer.title": "ウェブ開発者 <br/> & 大学生",
 };
 
 export default jp;
+
